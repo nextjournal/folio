efcb17f37e547fd378764168a470031b4a0916fb
Philippa Markovics <philippa@markovics.com>
2023-08-10 17:34:39 +0200

    First cut of client-side folio API

---
+++ demo/clerk_editor.cljs
@@ -0,0 +1,274 @@
+(ns clerk-editor
+  (:require ["@codemirror/commands" :refer [history historyKeymap]]
+            ["@codemirror/state" :refer [EditorState]]
+            ["@codemirror/view" :refer [keymap placeholder]]
+            ["react-dom/client" :as react-client]
+            [applied-science.js-interop :as j]
+            [clojure.string :as str]
+            [goog.object :as gobject]
+            [nextjournal.clerk.parser :as parser]
+            [nextjournal.clerk.render :as render]
+            [nextjournal.clerk.render.code :as code]
+            [nextjournal.clerk.render.editor :as editor]
+            [nextjournal.clerk.render.hooks :as hooks]
+            [nextjournal.clerk.render.panel :as panel]
+            [nextjournal.clerk.sci-env]
+            [nextjournal.clerk.viewer :as v]
+            [nextjournal.clojure-mode.extensions.eval-region :as eval-region]
+            [nextjournal.clojure-mode.keymap :as clojure-mode.keymap]
+            [nextjournal.command-bar :as command-bar]
+            [nextjournal.command-bar.keybind :as keybind]
+            [nextjournal.folio :as folio]
+            [reagent.core :as reagent]
+            [rewrite-clj.node :as n]
+            [rewrite-clj.parser :as p]
+            [sci.core :as sci]
+            [sci.ctx-store]))
+
+(defn format-1 [fmt-str x]
+  (str/replace-first fmt-str "%s" x))
+
+(defn sci-info [{:keys [sym ctx] ns-str :ns}]
+  (if-not sym
+    {:status ["no-eldoc" "done"]
+     :err "Message should contain a `sym`"}
+    (let [code (-> "(when-let [the-var (ns-resolve '%s '%s)] (meta the-var))"
+                   (format-1 ns-str)
+                   (format-1 sym))
+          [kind val] (try [::success (sci/eval-string* ctx code)]
+                          (catch :default e
+                            [::error (str e)]))
+          {:keys [doc file line name arglists]} val]
+      (if (and name (= kind ::success))
+        (cond-> {:ns (some-> val :ns ns-name)
+                 :arglists (pr-str arglists)
+                 :eldoc (mapv #(mapv str %) arglists)
+                 :arglists-str (.join (apply array arglists) "\n")
+                 :status ["done"]
+                 :name name}
+          doc (assoc :doc doc)
+          file (assoc :file file)
+          line (assoc :line line))
+        {:status ["done" "no-eldoc"]}))))
+
+(defn get-block-id [!id->count {:as block :keys [var form type doc]}]
+  (let [id->count @!id->count
+        id (if var
+             var
+             (let [hash-fn hash]
+               (symbol (str *ns*)
+                       (case type
+                         :code (str "anon-expr-" (hash-fn form))
+                         :markdown (str "markdown-" (hash-fn doc))))))]
+    (swap! !id->count update id (fnil inc 0))
+    (if (id->count id)
+      (symbol (str *ns*) (str (name id) "#" (inc (id->count id))))
+      id)))
+
+(defn analyze [form]
+  (cond-> {:form form}
+    (and (seq? form)
+         (str/starts-with? (str (first form)) "def"))
+    (assoc :var (second form))))
+
+(defn ns-resolver [notebook-ns]
+  (into {} (map (juxt key (comp ns-name val))) '{clerk nextjournal.clerk}))
+
+(defn parse-ns-aliases [ns-form]
+  (some (fn [x]
+          (when (and (seq? x)
+                     (= :require (first x)))
+            (into {}
+                  (keep (fn [require-form]
+                          (when (and (vector? require-form)
+                                     (= 3 (count require-form))
+                                     (contains? #{:as :as-alias} (second require-form)))
+                            ((juxt peek first) require-form))))
+                  (rest x))))
+        ns-form))
+
+;; TODO: unify with `analyzer/analyze-doc` and move to parser
+(defn analyze-doc
+  ([doc]
+   (analyze-doc {:doc? true} doc))
+  ([{:as state :keys [doc?]} doc]
+   (binding [*ns* *ns*]
+     (let [!id->count (atom {})]
+       (cond-> (reduce (fn [{:as state notebook-ns :ns :keys [ns-aliases]} i]
+                         (let [{:as block :keys [type text]} (get-in doc [:blocks i])]
+                           (if (not= type :code)
+                             (assoc-in state [:blocks i :id] (get-block-id !id->count block))
+                             (let [node (p/parse-string text)
+                                   form (try (n/sexpr node (when ns-aliases {:auto-resolve ns-aliases}))
+                                             (catch js/Error e
+                                               (throw (ex-info (str "Clerk analysis failed reading block: "
+                                                                    (ex-message e))
+                                                               {:block block
+                                                                :file (:file doc)}
+                                                               e))))
+                                   analyzed (cond-> (analyze form)
+                                              (:file doc) (assoc :file (:file doc)))
+                                   block-id (get-block-id !id->count (merge analyzed block))
+                                   analyzed (assoc analyzed :id block-id)]
+                               (cond-> state
+                                 (and (not ns-aliases) (parser/ns? form)) (assoc :ns-aliases (parse-ns-aliases form))
+                                 doc? (update-in [:blocks i] merge analyzed)
+                                 doc? (assoc-in [:blocks i :text-without-meta]
+                                                (parser/text-with-clerk-metadata-removed text (ns-resolver notebook-ns)))
+                                 (and doc? (not (contains? state :ns))) (merge (parser/->doc-settings form) {:ns *ns*}))))))
+                       (cond-> state
+                         doc? (merge doc))
+                       (-> doc :blocks count range))
+         doc? (-> parser/add-block-settings
+                  parser/add-open-graph-metadata
+                  parser/filter-code-blocks-without-form))))))
+
+(defn eval-blocks [doc]
+  (update doc :blocks (partial map (fn [{:as cell :keys [type text var form]}]
+                                     (cond-> cell
+                                       (= :code type)
+                                       (assoc :result
+                                              {:nextjournal/value (cond->> (eval form)
+                                                                    var (hash-map :nextjournal.clerk/var-from-def))}))))))
+
+(defn eval-notebook [code]
+  (->> code
+       (parser/parse-clojure-string {:doc? true})
+       (analyze-doc)
+       (eval-blocks)
+       (v/with-viewer v/notebook-viewer)
+       v/present))
+
+(def !eval-result (reagent/atom nil))
+(def !notebook (reagent/atom nil))
+
+(defn keys-view [spec]
+  (into [:span.inline-flex {:class "gap-[2px]"}]
+        (map (fn [k] [:span.rounded-sm.shadow.border.border-slate-300.shadow-inner.font-bold.leading-none.text-center
+                     {:class "px-[3px] py-[1px] min-w-[16px]"} k]))
+        (str/split (command-bar/get-pretty-spec spec) #" ")))
+
+(defn key-description [{:keys [codemirror? run spec var]}]
+  [:div.font-mono {:class "text-[12px]"}
+   [:div
+    [keys-view spec]
+    " is bound to "
+    (when codemirror?
+      [:span "CodeMirror's "])
+    [:span
+     (when-let [ns (some-> var meta :ns)]
+       [:span ns "/"])
+     [:span.font-bold (command-bar/get-fn-name run)]]
+    (when codemirror?
+      " command")]
+   (when-let [docs (some-> var meta :doc)]
+     [:div.mt-3 docs])])
+
+(defn doc-view [sym]
+  [:div.font-mono {:class "text-[12px]"}
+   (let [{:as info :keys [arglists-str doc ns name]} (sci-info {:sym sym :ns "user" :ctx (sci.ctx-store/get-ctx)})]
+     (if ns
+       [:div
+        [:div.flex.gap-2
+         [:div.font-bold ns "/" name]
+         [:div arglists-str]]
+        (let [bindings (keep (fn [{:as binding :keys [var]}]
+                               (when-let [{:keys [ns name]} (meta var)]
+                                 (when (and (= ns (:ns info)) (= name (:name info)))
+                                   binding)))
+                             @command-bar/!global-bindings)]
+          (when (seq bindings)
+            (into [:div.mt-3 "It is bound to "]
+                  (map-indexed (fn [i {:keys [spec]}]
+                                 [:<>
+                                  (when-not (zero? i)
+                                    [:span ", and "])
+                                  [keys-view spec]]))
+                  bindings)))
+        (when doc
+          [:div.mt-3 doc])]
+       [:div "No docs found for " [:span.font-bold sym] "."]))])
+
+(defn doc
+  "Shows bindings and doc for a given function."
+  []
+  (command-bar/toggle-interactive!
+   (fn [!state]
+     (hooks/use-effect
+      (fn []
+        (keybind/disable!)
+        #(keybind/enable!)))
+     [:<>
+      [command-bar/label {:text "Which name:"}]
+      [command-bar/input !state {:placeholder "Enter name…"
+                                 :default-value (or (:doc/name @!state) "")
+                                 :on-key-down (fn [event]
+                                                (when (= (.-key event) "Enter")
+                                                  (swap! !eval-result assoc :result (reagent/as-element [doc-view (:doc/name @!state)])))
+                                                (when (contains? #{"Escape" "Enter"} (.-key event))
+                                                  (.preventDefault event)
+                                                  (.stopPropagation event)
+                                                  (command-bar/kill-interactive!)
+                                                  (swap! !state dissoc :doc/name)))
+                                 :on-input (fn [event]
+                                             (swap! !state assoc :doc/name (.. event -target -value)))}]])))
+
+(defn describe-key
+  "Describes which function a key or sequence of keys is bound to. Shows the bound function's docstring when available."
+  []
+  (command-bar/toggle-interactive!
+   (fn [!state]
+     (hooks/use-effect
+      (fn []
+        (keybind/disable!)
+        #(keybind/enable!)))
+     [:<>
+      [command-bar/label {:text "Describe key:"}]
+      [command-bar/input !state {:placeholder "Press a key or binding…"
+                                 :default-value (if-let [spec (:describe-key/spec @!state)]
+                                                  (command-bar/get-pretty-spec spec)
+                                                  "")
+                                 :on-key-down (fn [event]
+                                                (.preventDefault event)
+                                                (.stopPropagation event)
+                                                (if (= (.-key event) "Escape")
+                                                  (command-bar/kill-interactive!)
+                                                  (let [chord (keybind/e->chord event)
+                                                        spec (cond-> chord
+                                                               (command-bar/mod-only-chord? chord) (dissoc :button)
+                                                               true command-bar/chord->spec)]
+                                                    (swap! !state assoc :describe-key/spec spec))))
+                                 :on-key-up (fn [event]
+                                              (when-let [spec (:describe-key/spec @!state)]
+                                                (when-let [binding (command-bar/get-binding-from-spec spec)]
+                                                  (swap! !eval-result assoc :result (reagent/as-element [key-description binding])))
+                                                (swap! !state dissoc :describe-key/spec)
+                                                (command-bar/kill-interactive!)))}]])))
+
+(defn editor []
+  (let [!state (hooks/use-state {:commands {"Alt-d" #'describe-key
+                                            "Shift-Alt-d" #'doc}
+                                 :doc ""
+                                 :on-change-selection #(reset! !eval-result nil)
+                                 :on-eval-doc #(reset! !notebook (eval-notebook %))
+                                 :on-eval-expr #(reset! !eval-result (try {:result (sci/eval-string* (sci.ctx-store/get-ctx) %)}
+                                                                          (catch js/Error e {:error (ex-message e)})))})]
+    [:<>
+     [folio/split
+      [folio/editor @!state]
+      (when-some [notebook @!notebook]
+        [:div
+         [:> render/ErrorBoundary {:hash (gensym)}
+          [render/inspect-presented notebook]]])]
+     (when-let [{:keys [error result]} @!eval-result]
+       [folio/display (cond
+                        error [:div.text-red-600.text-xs.font-mono error]
+                        (render/valid-react-element? result) result
+                        :else (render/inspect result))])]))
+
+(defonce react-root
+  (react-client/createRoot (js/document.getElementById "root")))
+
+(defn ^:dev/after-load render []
+  (when react-root
+    (.render react-root (reagent/as-element [editor]))))
---
+++ src/nextjournal/folio.cljs
@@ -0,0 +1,183 @@
+(ns nextjournal.folio
+  (:require ["@codemirror/commands" :refer [history historyKeymap]]
+            ["@codemirror/language" :refer [HighlightStyle syntaxHighlighting LanguageDescription]]
+            ["@codemirror/state" :refer [Compartment EditorState RangeSet RangeSetBuilder Text]]
+            ["@codemirror/view" :refer [EditorView Decoration keymap placeholder]]
+            ["@lezer/highlight" :refer [tags highlightTree]]
+            ["@nextjournal/lang-clojure" :refer [clojureLanguage]]
+            ["react" :as react]
+            ["react-dom/client" :as react-client]
+            [applied-science.js-interop :as j]
+            [nextjournal.folio.localstorage :as localstorage]
+            [nextjournal.clojure-mode :as clojure-mode]
+            [nextjournal.clojure-mode.extensions.eval-region :as eval-region]
+            [nextjournal.clojure-mode.keymap :as clojure-mode.keymap]
+            [nextjournal.command-bar :as command-bar]
+            [reagent.core :as reagent]))
+
+(def bar-height 26)
+
+(def local-storage-dark-mode-key "darkmode")
+
+(def !dark-mode?
+  (reagent/atom (boolean (localstorage/get-item local-storage-dark-mode-key))))
+
+(defn set-dark-mode! [dark-mode?]
+  (let [class-list (.-classList (js/document.querySelector "html"))]
+    (if dark-mode?
+      (.add class-list "dark")
+      (.remove class-list "dark")))
+  (localstorage/set-item! local-storage-dark-mode-key dark-mode?))
+
+(defn setup-dark-mode! []
+  (add-watch !dark-mode? ::dark-mode-watch
+             (fn [_ _ old dark-mode?]
+               (when (not= old dark-mode?)
+                 (set-dark-mode! dark-mode?))))
+  (when @!dark-mode?
+    (set-dark-mode! @!dark-mode?)))
+
+(def theme (Compartment.))
+
+(defn get-theme []
+  (.theme EditorView
+          (j/lit {"&.cm-focused" {:outline "none"}
+                  ".cm-line" {:padding "0"
+                              :line-height "1.6"
+                              :font-size "15px"
+                              :font-family "\"Fira Mono\", monospace"}
+                  ".cm-matchingBracket" {:border-bottom "1px solid var(--teal-color)"
+                                         :color "inherit"}
+
+                  ;; only show cursor when focused
+                  ".cm-cursor" {:visibility "hidden"}
+                  "&.cm-focused .cm-cursor" {:visibility "visible"
+                                             :animation "steps(1) cm-blink 1.2s infinite"}
+                  "&.cm-focused .cm-selectionBackground" {:background-color "Highlight"}
+                  ".cm-tooltip" {:border "1px solid rgba(0,0,0,.1)"
+                                 :border-radius "3px"
+                                 :overflow "hidden"}
+                  ".cm-tooltip > ul > li" {:padding "3px 10px 3px 0 !important"}
+                  ".cm-tooltip > ul > li:first-child" {:border-top-left-radius "3px"
+                                                       :border-top-right-radius "3px"}
+                  ".cm-tooltip.cm-tooltip-autocomplete" {:border "0"
+                                                         :border-radius "6px"
+                                                         :box-shadow "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)"
+                                                         "& > ul" {:font-size "12px"
+                                                                   :font-family "'Fira Code', monospace"
+                                                                   :background "rgb(241 245 249)"
+                                                                   :border "1px solid rgb(203 213 225)"
+                                                                   :border-radius "6px"}}
+                  ".cm-tooltip-autocomplete ul li[aria-selected]" {:background "rgb(79 70 229)"
+                                                                   :color "#fff"}
+                  ".cm-tooltip.cm-tooltip-hover" {:background "rgb(241 245 249)"
+                                                  :border-radius "6px"
+                                                  :border "1px solid rgb(203 213 225)"
+                                                  :box-shadow "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)"
+                                                  :max-width "550px"}}) #js {:dark @!dark-mode?}))
+
+(defn use-dark-mode [editor-view]
+  (react/useEffect
+   (fn []
+     (add-watch !dark-mode? ::dark-mode #(.dispatch editor-view #js {:effects (.reconfigure theme (get-theme))}))
+     #(remove-watch !dark-mode? ::dark-mode))
+   #js[]))
+
+(def highlight-style
+  (.define HighlightStyle
+           (clj->js [{:tag (.-meta tags) :class "cmt-meta"}
+                     {:tag (.-link tags) :class "cmt-link"}
+                     {:tag (.-heading tags) :class "cmt-heading"}
+                     {:tag (.-emphasis tags) :class "cmt-italic"}
+                     {:tag (.-strong tags) :class "cmt-strong"}
+                     {:tag (.-strikethrough tags) :class "cmt-strikethrough"}
+                     {:tag (.-keyword tags) :class "cmt-keyword"}
+                     {:tag (.-atom tags) :class "cmt-atom"}
+                     {:tag (.-bool tags) :class "cmt-bool"}
+                     {:tag (.-url tags) :class "cmt-url"}
+                     {:tag (.-contentSeparator tags) :class "cmt-contentSeparator"}
+                     {:tag (.-labelName tags) :class "cmt-labelName"}
+                     {:tag (.-literal tags) :class "cmt-literal"}
+                     {:tag (.-inserted tags) :class "cmt-inserted"}
+                     {:tag (.-string tags) :class "cmt-string"}
+                     {:tag (.-deleted tags) :class "cmt-deleted"}
+                     {:tag (.-regexp tags) :class "cmt-regexp"}
+                     {:tag (.-escape tags) :class "cmt-escape"}
+                     {:tag (.. tags (special (.-string tags))) :class "cmt-string"}
+                     {:tag (.. tags (definition (.-variableName tags))) :class "cmt-variableName"}
+                     {:tag (.. tags (local (.-variableName tags))) :class "cmt-variableName"}
+                     {:tag (.-typeName tags) :class "cmt-typeName"}
+                     {:tag (.-namespace tags) :class "cmt-namespace"}
+                     {:tag (.-className tags) :class "cmt-className"}
+                     {:tag (.. tags (special (.-variableName tags))) :class "cmt-variableName"}
+                     {:tag (.-macroName tags) :class "cmt-macroName"}
+                     {:tag (.. tags (definition (.-propertyName tags))) :class "cmt-propertyName"}
+                     {:tag (.-comment tags) :class "cmt-comment"}
+                     {:tag (.-invalid tags) :class "cmt-invalid"}])))
+
+(defn eval-region* [get-region-fn eval-fn editor-state]
+  (when-some [code-str (get-region-fn editor-state)]
+    (eval-fn code-str))
+  true)
+
+(defn on-change-ext [{:keys [on-change on-change-selection]}]
+  (.. EditorState -transactionExtender
+      (of (fn [^js tr]
+            (when (and on-change (.-docChanged tr)) (on-change tr))
+            (when (and on-change-selection (.-selection tr)) (on-change-selection tr))
+            #js {}))))
+
+(defn default-extensions [{:as state :keys [on-eval-doc on-eval-expr] placeholder-text :placeholder}]
+  #js [clojure-mode/default-extensions
+       (syntaxHighlighting highlight-style)
+       (.of theme (get-theme))
+       (placeholder placeholder-text)
+       (history)
+       command-bar/extension
+       (eval-region/extension {:modifier "Meta"})
+       (on-change-ext state)
+       (.of keymap historyKeymap)
+       (.of keymap clojure-mode.keymap/complete)
+       (.of keymap
+            (j/lit
+             [{:key "Alt-Enter"
+               :run (fn eval-doc [editor-view]
+                      (on-eval-doc (.. editor-view -state -doc toString)))}
+              {:key "Mod-Enter"
+               :shift (fn eval-top-level-form [editor-view]
+                        (eval-region* eval-region/top-level-string on-eval-expr (.-state editor-view)))
+               :run (fn eval-form-at-cursor [editor-view]
+                      (eval-region* eval-region/cursor-node-string on-eval-expr (.-state editor-view)))}]))])
+
+(defn make-view [state parent]
+  (EditorView. (j/obj :state state :parent parent)))
+
+(defn make-state [{:as state :keys [doc extensions on-change]}]
+  (.create EditorState (j/obj :doc doc :extensions (cond-> (default-extensions state)
+                                                     (seq extensions) (.concat extensions)))))
+
+(defn display [& content]
+  (into
+   [:div.border-t.border-slate-300.dark:border-slate-600.px-4.py-2.flex-shrink-0.absolute.left-0.w-screen.bg-white.dark:bg-slate-950
+    {:style {:box-shadow "0 -2px 3px 0 rgb(0 0 0 / 0.025)" :bottom bar-height}}]
+   content))
+
+(defn split [& content]
+  (into [:div.grid {:class (str "grid-cols-" (count (remove nil? content)))}] content))
+
+(defn editor [{:as state :keys [commands]}]
+  (let [editor-el (react/useRef nil)
+        [editor-view set-editor-view!] (react/useState nil)]
+    (react/useEffect
+     (fn []
+       (let [editor-view* (make-view (make-state state) (.-current editor-el))]
+         (set-editor-view! editor-view*)
+         #(.destroy editor-view*)))
+     #js[])
+    (use-dark-mode editor-view)
+    [:<>
+     [:div.bg-slate-200.border-r.border-slate-300.dark:border-slate-600.px-4.py-3.dark:bg-slate-950.overflow-y-auto.relative
+      {:style {:height (str "calc(100vh - " bar-height "px)")}}
+      [:div.w-full.h-screen {:ref editor-el}]]
+     [:div.fixed.left-0.bottom-0.w-screen {:style {:height bar-height}}
+      [command-bar/view (merge {} commands)]]]))
